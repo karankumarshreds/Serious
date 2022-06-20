aa3309db946aaa45d12d2914022c660dcc340cb5
karankumarshreds <naileditnerd@gmail.com>
2022-06-20 19:40:12 +0530

    04 sol 2

--- 04_container.js
+++ 04_container.js
@@ -13,4 +13,43 @@ function biggestContainer(array) {
     return maxArea;
 }
 
-console.log(biggestContainer([1, 5, 6, 3, 4]));
+// console.log(biggestContainer([1, 5, 6, 3, 4]));
+
+/**
+ * |         6
+ * |     5   |
+ * |     |   |       4
+ * |     |   |   3   |
+ * |     |   |   |   |
+ * | 1   |   |   |   |
+ * _ | _ | _ | _ | _ | _
+ *   0   1   2   3   4
+ */
+
+var startTime = new Date().getTime();
+
+function biggestContainer2(array) {
+    // start with extremes and move the only extreme pointer towards center
+    // only if the next one is bigger from the current one
+    let li = 0;
+    let ri = array.length - 1;
+    let area = -Infinity;
+    do {
+        let width = Math.abs(li - ri);
+        let height = Math.min(array[li], array[ri]);
+        area = Math.max(area, height * width);
+        console.log({ width, height, leftIndexRightIndex: li === ri });
+        if (array[li + 1] > array[li]) {
+            li++;
+        } else if (array[ri - 1] > array[ri]) {
+            ri--;
+        } else {
+            li++;
+            ri--;
+        }
+    } while (li !== ri);
+    return area;
+}
+var endTime = new Date().getTime();
+
+console.log(Math.abs(startTime - endTime));
