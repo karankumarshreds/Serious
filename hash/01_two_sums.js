89c2ed49502491388ba5af2db924a28dbcaad352
karankumarshreds <naileditnerd@gmail.com>
2022-06-22 22:06:57 +0530

    add sol 1

---
+++ hash/01_two_sums.js
@@ -0,0 +1,18 @@
+// Question 1: Two Sum - You are given an array of Integers and another integer targetValue. 
+// Write a function that will take these inputs and return the indices of the 2 integers in the array that add up targetValue.
+
+
+function twoSums01(array, sum) {
+	// 1,3
+	const indeces = []
+	for (let i=0; i<array.length; i++) {
+		if (indeces.length === 2) break;
+		const counterPart = (sum - array[i])	
+		if (array.includes(counterPart)){
+			indeces.push(array.indexOf(counterPart))
+		}
+	}
+	return indeces;
+}
+
+console.log(twoSums01([3,5,2,9],14))
