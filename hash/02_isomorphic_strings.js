c444af472e8978709e21cc4723f26b55cd459423
karankumarshreds <naileditnerd@gmail.com>
2022-06-23 15:15:52 +0530

    update

---
+++ hash/02_isomorphic_strings.js
@@ -0,0 +1,16 @@
+// Question 2: Isomorphic Strings - Given two strings s and t, determine if they are isomorphic. 
+// Two strings s and t are isomorphic if the characters in s can be replaced to get t. 
+// All occurrences of a character must be replaced with another character while preserving the order of characters. 
+// No two characters may map to the same character, but a character may map to itself. s and t consist of any valid ascii character.
+
+
+function isIsomorphic(str1, str2) {
+	// abacus
+	// rurfgs 
+	const map = {}
+	if (str1.length !== str2.length) return false 
+	for (let i=0; i<str1.length; i++) {
+		map[str1[i]] = str2[i]
+
+	}
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+ C:\Users\illum\AppData\Local\nvim> // path for the nvim configs 
+ ```:PlugInstall```
